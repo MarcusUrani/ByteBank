a2eca04864367e4f0b759e53dcf396009d51180a
Marcus Urani <marcus.urani20@gmail.com>
2021-09-11 19:28:31 -0300

    Criando classe de conta corrente, adicionando funções dentro das classes para saque e depósito e utilizando early return

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-class cliente{
+class cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 }
 
-const cliente1 = new cliente;
-const cliente2 = new cliente;
+class contaCorrente {
+    agencia;
+    saldo;
 
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            console.log("Saque realizado com sucesso, seu saldo agora é de " + this.saldo + " Reais");
+            return valor;
+        } else {
+            console.log("O seu saldo não é suficiente pra realizar essa operação");
+        }
+    }
+
+    depositar(valor) {
+        if(valor <= 1) {
+            return;
+        } 
+        this.saldo += valor;
+        console.log("Depósito realizado com sucesso, seu saldo agora é de " + this.saldo + " reais");
+    }
+}
+
+const cliente1 = new cliente;
 cliente1.nome = "Marcus";
 cliente1.cpf = 05135163107;
-cliente1.agencia = 101;
-cliente1.saldo = 0;
 cliente1.rg = 3860942;
 
-cliente2.nome = "Cinira";
-cliente2.cpf = 51202417191;
-cliente2.agencia = 1001;
-cliente2.saldo = 3500.00;
-cliente2.rg = 4523942;
 
-console.log(cliente1, "\n\n", cliente2);
+// const cliente2 = new cliente;
+// cliente2.nome = "Cinira";
+// cliente2.cpf = 51202417191;
+// cliente2.rg = 4523942;
+
+const contaCorrenteMarcus = new contaCorrente;
+contaCorrenteMarcus.agencia = 101;
+contaCorrenteMarcus.saldo = 100;
+console.log(contaCorrenteMarcus.saldo + " Reais");
+
+
+contaCorrenteMarcus.sacar(100);
+contaCorrenteMarcus.depositar(100);
