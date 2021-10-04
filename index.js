d1ee4303da08147eaca2146c7f4b1a98f89800c8
Marcus Urani <marcus.urani20@gmail.com>
2021-10-04 20:33:15 -0300

    Adicionando as funcionalidades da conta salário, e iniciando a criação dos funcionários

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { Cliente } from "../ByteBank/Cliente.js";
-import { ContaCorrente } from "../ByteBank/ContaCorrente.js";
-import { ContaPoupanca } from "../ByteBank/ContaPoupanca.js";
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
 
 const cliente1 = new Cliente("Marcus Urani", "05135163107", 3860942);
 // const cliente2 = new cliente("Jade Eymard", "51124463109", 4523460);
@@ -14,9 +15,14 @@ const conta1 = new ContaCorrente(cliente1, 101);
 
 const contaPoupanca = new ContaPoupanca(100, cliente1, 101);
 
-conta1.depositar(500);
-conta1.sacar(100);
-contaPoupanca.sacar(20);
+const contaSalario = new ContaSalario(cliente1);
 
-console.log(contaPoupanca);
+
+contaPoupanca.sacar(20);
+conta1.sacar(200);
+contaSalario.depositar(5000);
+contaSalario.sacar(500);
+
 console.log(conta1);
+console.log(contaPoupanca);
+console.log(contaSalario);
